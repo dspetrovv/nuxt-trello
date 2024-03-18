d3ff8446202f8b5ed54c6a32f80e46c243673a13
dspetrovv <dspetrovv@yandex.ru>
2024-03-18 14:24:50 +0300

    add colors and index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-  <div></div>
+  <div class="columns">
+    <div class="columns__column">
+      <section class="columns__header">
+        On holds ({{ count }})
+      </section>
+      <section class="columns__cards">
+        task-card
+      </section>
+      <section class="columns__buttons">
+        <button></button>
+      </section>
+    </div>
+  </div>
 </template>
